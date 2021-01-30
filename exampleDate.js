a4ef52eda3140b1ec718dff715c8e069493545a3
Ray Lin <raylin009@gmail.com>
2021-01-30 01:25:10 -0800

    added example for input field id

---
+++ exampleDate.js
@@ -0,0 +1,60 @@
+const PDFFieldList = `PDFTextField: 3 DMOS
+PDFTextField: 5 SSN
+PDFTextField: 7 No of Drills Missed
+PDFTextField: 6 Dates of AbsenceRow1
+PDFTextField: EveningRow1
+PDFTextField: 6 Dates of AbsenceRow2
+PDFTextField: EveningRow2
+PDFTextField: 6 Dates of AbsenceRow3
+PDFTextField: EveningRow3
+PDFTextField: 8 REQUEST a X Rescheduled Training RST Complete blocks 12 thru 20 b Equivalent Training ET Complete blocks 13 thru 20 c Excused Absence No MakeUp Complete blocks 19 thru 20
+PDFTextField: 9 Reason for Absence
+PDFTextField: 10 Signature of Applicant
+PDFTextField: 11 Date
+PDFTextField: 14  Location of Training
+PDFTextField: 19  Supervisors Signature
+PDFTextField: 20 Date
+PDFTextField: 22  First Sergeants Signature
+PDFTextField: 23 Date
+PDFTextField: 24  Commanders Signature
+PDFTextField: 25 Date
+PDFSignature: Signature2
+PDFSignature: Signature3
+PDFSignature: Signature4
+PDFTextField: b
+PDFTextField: TO
+PDFTextField: LOCATION
+PDFTextField: REMARKS
+PDFTextField: PLATOON LEADER  SERGEANT SIGNATURE
+PDFTextField: DATE_2
+PDFTextField: DATE
+PDFTextField: RST SUPERVISOR SIGNATURE
+PDFTextField: 4 RANK
+PDFTextField: 1 NAME Last First MI
+PDFTextField: 2 Section and Unit of Assignment
+PDFTextField: 13 Date of Makeup
+PDFTextField: 16 Time of Training FROM
+PDFTextField: 15 Training will be supervised by
+PDFCheckBox: Check Box3
+PDFCheckBox: Check Box4
+PDFCheckBox: Check Box5
+PDFCheckBox: Check Box6
+PDFCheckBox: Check Box7
+PDFCheckBox: Check Box8
+PDFCheckBox: Check Box9
+PDFCheckBox: Check Box10
+PDFTextField: 16 Time of Training TO
+PDFTextField: DUTIES TO BE PERFORMEDRow1
+PDFTextField: DUTIES TO BE PERFORMEDRow2
+PDFTextField: DUTIES TO BE PERFORMEDRow3
+PDFTextField: DUTIES TO BE PERFORMEDRow4
+PDFTextField: DUTIES TO BE PERFORMEDRow5
+PDFTextField: DUTIES TO BE PERFORMEDRow6
+PDFTextField: DUTIES TO BE PERFORMEDRow7
+PDFSignature: Signature5
+PDFSignature: Signature6
+PDFSignature: Signature7
+PDFCheckBox: Check Box1
+PDFCheckBox: Check Box2
+PDFCheckBox: Check Box11
+PDFCheckBox: Check Box12`
